f8f2ceea894ca4cb9f53933674caf7f9b0d46b46
Kerem <nkcoskun@gmail.com>
2023-05-10 21:53:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/landmarkbookjava/DetailsActivity.java
@@ -0,0 +1,30 @@
+package com.example.landmarkbookjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.landmarkbookjava.databinding.ActivityDetailsBinding;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private ActivityDetailsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Intent intent = getIntent();
+
+        Landmark selectedLandmark = (Landmark) intent.getSerializableExtra("landmark");
+
+        binding.nameText.setText(selectedLandmark.name);
+        binding.countryText.setText(selectedLandmark.country);
+        binding.imageView.setImageResource(selectedLandmark.image);
+    }
+}
